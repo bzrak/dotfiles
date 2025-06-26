71aebf1558b7533d96209bc16d6b1e0b29315c64
bzrak <67083456+bzrak@users.noreply.github.com>
2025-06-26 02:13:17 -0300

    Modified lazyvim rootdir and cwd default behavior

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -2,8 +2,19 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-local opt = vim.opt
+-- Remove LazyVim defaut option of identifying the root directory.
+vim.g.root_spec = { "cwd" }
 
+local opt = vim.opt
+-- Options
 opt.tabstop = 4
 opt.softtabstop = 4
 opt.shiftwidth = 4
+
+-- Python
+--
+-- LSP Server to use for Python.
+-- Set to "basedpyright" to use basedpyright instead of pyright.
+vim.g.lazyvim_python_lsp = "pyright"
+-- Set to "ruff_lsp" to use the old LSP implementation version.
+vim.g.lazyvim_python_ruff = "ruff"
---
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -0,0 +1,9 @@
+return {
+  "nvim-neo-tree/neo-tree.nvim",
+  opts = {
+    filesystem = {
+      bind_to_cwd = true,
+      cwd_target = { "global" },
+    },
+  },
+}
