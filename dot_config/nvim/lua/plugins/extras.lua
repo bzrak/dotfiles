491999e122764d4c2f1620dcee2bb9fe92657097
bzrak <67083456+bzrak@users.noreply.github.com>
2025-12-07 12:51:35 -0300

    chore(nvim) add plugin

--- dot_config/nvim/lua/plugins/extras.lua
+++ dot_config/nvim/lua/plugins/extras.lua
@@ -1,5 +1,15 @@
-if true then
-  return {}
-end
+-- if true then return {} end
 
--- { import = "lazyvim.plugins.extras.ui.mini-starter" }
+return {
+  { import = "lazyvim.plugins.extras.lang.clangd" },
+  { import = "lazyvim.plugins.extras.lang.docker" },
+  { import = "lazyvim.plugins.extras.lang.markdown" },
+  { import = "lazyvim.plugins.extras.lang.python" },
+  { import = "lazyvim.plugins.extras.util.chezmoi" },
+  { import = "lazyvim.plugins.extras.util.dot" },
+  { import = "lazyvim.plugins.extras.util.mini-hipatterns" },
+  { import = "lazyvim.plugins.extras.lang.typescript" },
+  { import = "lazyvim.plugins.extras.lang.tailwind" },
+  { import = "lazyvim.plugins.extras.lang.json" },
+  { import = "lazyvim.plugins.extras.editor.inc-rename" },
+}
